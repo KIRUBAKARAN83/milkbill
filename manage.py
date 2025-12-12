4c42d71107a9fe969240c9752cdeb4191cb46a9d
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2025-12-12 16:57:06 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'milk_billing_complete\milkproject.settings')
+   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'milkproject.settings')
 
     try:
         from django.core.management import execute_from_command_line
