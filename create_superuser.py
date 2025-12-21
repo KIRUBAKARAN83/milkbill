9b14a07b4a15fd6a180527d27e565003620811a9
D.KIRUBAKARAN <dhayalankiruba17@gmail.com>
2025-12-21 21:58:43 +0530

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -8,24 +8,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+USERNAME = "kiruba_karan"
+PASSWORD = "kiruba"
 
-if not username or not password:
-    print("Missing env vars")
-else:
-    user, created = User.objects.get_or_create(
-        username=username,
-        defaults={"email": email}
-    )
+user, created = User.objects.get_or_create(username=USERNAME)
 
-    user.set_password(password)
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
+user.set_password(PASSWORD)
+user.is_staff = True
+user.is_superuser = True
+user.save()
 
-    if created:
-        print("Superuser created")
-    else:
-        print("Superuser password updated")
+print("ADMIN READY | created =", created)
